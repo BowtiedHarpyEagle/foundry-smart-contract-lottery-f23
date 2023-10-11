3d27a80c45afdbdf6b3c1d0cc0c3ddffce52e7fb
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2023-10-11 13:45:15 -0500

    complete L9P1

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -6,10 +6,23 @@ pragma solidity ^0.8.18;
  * @author BowtiedHarpyEagle
  * @notice this is a sample contract of a raffle based on Patrick Collins' tutorial 
  * on YouTube (https://www.youtube.com/watch?v=gecEjRVNt34&list=PL2-Nvp2Kn0FPH2xU3IbKrrkae-VVXs1vk&index=107)
+ * @dev implements Chainlink VRFv2 
  */
 
 contract Raffle {
-    constructor() {
-        
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
     }
+
+    function enterRaffle()  public payable {}
+
+    function pickWinner() public {}
+
+    /** Getter Function */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    } 
+    
 }
