c1919beb8488b85db9d11538d12ef855a6fabb85
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2023-10-11 08:52:42 -0500

    L9P1 8:54 add comments

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+/**
+ * @title sample Raffle contract
+ * @author BowtiedHarpyEagle
+ * @notice this is a sample contract of a raffle based on Patrick Collins' tutorial 
+ * on YouTube (https://www.youtube.com/watch?v=gecEjRVNt34&list=PL2-Nvp2Kn0FPH2xU3IbKrrkae-VVXs1vk&index=107)
+ */
+
 contract Raffle {
     constructor() {
         
